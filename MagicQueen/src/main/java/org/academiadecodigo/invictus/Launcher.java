a826138c1188a3c09d8fc2948161d3cbced02c27
Mariana Dias <marianagdias94@gmail.com>
2018-12-13 20:05:31 +0000

    Made the Manager and the Launcher.

--- MagicQueen/src/main/java/org/academiadecodigo/invictus/Launcher.java
+++ MagicQueen/src/main/java/org/academiadecodigo/invictus/Launcher.java
@@ -4,6 +4,10 @@ package org.academiadecodigo.invictus;
 public class Launcher {
 
     public static void main(String[] args) {
+        Manager manager = new Manager();
+        manager.init();
+        manager.start();
+
     }
 
 
